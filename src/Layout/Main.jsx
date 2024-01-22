e9c73d32f3675825a0db1b09046a776705b8426f
mahfuzar175 <mahfuzar175@gmail.com>
2024-01-23 02:33:18 +0600

    add a search ber to the home page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
+import Nabvar from "../Pages/Shared/Nabvar";
 
 const Main = () => {
-    return (
-        <div className="flex flex-col min-h-screen">
-        <div className="flex-grow">
-          <Outlet></Outlet>
-        </div>
-       <Footer></Footer>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Nabvar></Nabvar>
+      <div className="flex-grow">
+        <Outlet></Outlet>
       </div>
-    );
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
